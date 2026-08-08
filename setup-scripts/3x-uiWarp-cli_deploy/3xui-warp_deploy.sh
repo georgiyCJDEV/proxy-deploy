7e6905fc86c5d6ddfe3ec6388c2060db932c2034
georgiyDev <156007110+georgiyCJDEV@users.noreply.github.com>
2026-08-09 01:45:28 +0300

    update 3xui-warp_deploy.sh

--- setup-scripts/3x-uiWarp-cli_deploy/3xui-warp_deploy.sh
+++ setup-scripts/3x-uiWarp-cli_deploy/3xui-warp_deploy.sh
@@ -70,7 +70,7 @@ if [[ ! -f ${XUI_DB} ]]; then
     cp "${XUI_BASE_DB}" "${XUI_DB_DIR}"
     log "--- Prepared base x-ui.db file: ${XUI_DB} ---"
 else
-    log "--- x-ui.db file already exists: ${XUI_DB} ---"
+    log "--- x-ui.db file already exists: $(ls -la ${XUI_DB_DIR}) ---"
 fi
 
 log "--- Downloading 3xui + warp_cli containers sources to ${XUI_SOURCES_GIT} ---"
