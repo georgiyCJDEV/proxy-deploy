bc737297c7e6931f990bfbec6e361e753fca7d94
georgiyDev <156007110+georgiyCJDEV@users.noreply.github.com>
2026-08-22 02:14:38 +0300

    updated update-xui-settings.sql

--- setup-scripts/3x-uiWarp-cli_deploy/update-xui-settings.sql
+++ setup-scripts/3x-uiWarp-cli_deploy/update-xui-settings.sql
@@ -2,12 +2,12 @@
 -- Params should be passed through .param set before running transaction
 -- Usage: sqlite3 x-ui.db < update-xui-settings.sql
 
-BEGIN TRANSACTION;
-
-UPDATE settings SET value = ''           WHERE key = 'webListen';
-UPDATE settings SET value = ''           WHERE key = 'webDomain';
-UPDATE settings SET value = :webPort     WHERE key = 'webPort';
-UPDATE settings SET value = :certFile    WHERE key = 'webCertFile';
-UPDATE settings SET value = :keyFile     WHERE key = 'webKeyFile';
-
-COMMIT;
+UPDATE settings
+SET value = CASE
+                WHEN key = 'webListen' THEN ''
+                WHEN key = 'webDomain' THEN ''
+                WHEN key = 'webPort' THEN :webPort
+                WHEN key = 'webCertFile' THEN :certFile
+                WHEN key = 'webKeyFile' THEN :keyFile
+    END
+WHERE key IN ('webListen', 'webDomain', 'webPort', 'webCertFile', 'webKeyFile');
